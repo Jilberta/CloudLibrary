548b156751cfd5a351ee40980f7326e8a9594b8a
mda <jilbertaz@yahoo.com>
2014-03-15 00:33:53 +0400

    Lokaciis amogebis Test

--- src/com/example/cloudlibrary/MainActivity.java
+++ src/com/example/cloudlibrary/MainActivity.java
@@ -9,39 +9,47 @@ import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-	}
+    }
 
-	public void onClick(View v) {
-		Intent k;
-		switch (v.getId()) {
-		case R.id.books:
-			Toast.makeText(this, "books", Toast.LENGTH_LONG).show();
-			k = new Intent(MainActivity.this, BooksListActivity.class);
-			startActivity(k);
-			break;
-		case R.id.comments:
-			Toast.makeText(this, "comments", Toast.LENGTH_LONG).show();
-			//k = new Intent(MainActivity.this, CommentsActivity.class);
-			//startActivity(k);
-			break;
-		case R.id.test:
-			Toast.makeText(this, "test", Toast.LENGTH_LONG).show();
-			break;
-		default:
-			break;
-		}
-	}
+    public void onClick(View v) {
+        Intent k;
+        switch (v.getId()) {
+            case R.id.books:
+                Toast.makeText(this, "books", Toast.LENGTH_LONG).show();
+                k = new Intent(MainActivity.this, BooksListActivity.class);
+                startActivity(k);
+                break;
+            case R.id.comments:
+                Toast.makeText(this, "comments", Toast.LENGTH_LONG).show();
+                //k = new Intent(MainActivity.this, CommentsActivity.class);
+                //startActivity(k);
+                break;
+            case R.id.test:
+                GPSTracker gps = new GPSTracker(this);
+                if (gps.canGetLocation()) {
+                    double latitude = gps.getLatitude();
+                    double longitude = gps.getLongitude();
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
+                    Toast.makeText(this, "Your Location is: \nLatitude: " + latitude + "\nLongitude: " + longitude, Toast.LENGTH_LONG).show();
+                }else{
+                    gps.showSettingsAlert();
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
 
 }

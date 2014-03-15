5331cbac31c75c223f207f234662af497be3c269
Kote Ujmajuridze <kujma10@freeuni.edu.ge>
2014-03-16 03:36:20 +0400

    ty

--- src/com/example/cloudlibrary/activities/BookInfoActivity.java
+++ src/com/example/cloudlibrary/activities/BookInfoActivity.java
@@ -1,6 +1,7 @@
 package com.example.cloudlibrary.activities;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.MalformedURLException;

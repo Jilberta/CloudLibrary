957298dd38973a1e311433f1623370e5f8691461
Kote Ujmajuridze <kujma10@freeuni.edu.ge>
2014-03-15 23:18:02 +0400

    downloading pdf and opening

--- src/com/example/cloudlibrary/activities/BookInfoActivity.java
+++ src/com/example/cloudlibrary/activities/BookInfoActivity.java
@@ -1,15 +1,25 @@
 package com.example.cloudlibrary.activities;
 
+import java.io.File;
 
+import com.example.cloudlibrary.helpers.ServiceAddresses;
 import com.example.cloudlibrary.model.Book;
 import com.example.cloudlibrary.qrcode.Contents;
 import com.example.cloudlibrary.qrcode.QRCodeEncoder;
 import com.google.zxing.BarcodeFormat;
 import com.google.zxing.WriterException;
 import android.app.Activity;
+import android.app.DownloadManager;
+import android.app.DownloadManager.Request;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.graphics.Bitmap;
 import android.graphics.Point;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.Display;
 import android.view.View;
 import android.view.WindowManager;
@@ -20,6 +30,8 @@ import android.widget.Toast;
 public class BookInfoActivity extends Activity {
 
 	private Book b;
+	private DownloadManager downloadmanager;
+	private long reference;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +52,7 @@ public class BookInfoActivity extends Activity {
 	public void onClick(View v) {
 		switch (v.getId()) {
 		case R.id.download:
+			downloadBook();
 			Toast.makeText(this, "download", Toast.LENGTH_SHORT).show();
 			break;
 		case R.id.qrCode:
@@ -48,6 +61,38 @@ public class BookInfoActivity extends Activity {
 		default:
 			break;
 		}
+	}
+
+	BroadcastReceiver onComplete = new BroadcastReceiver() {
+		public void onReceive(Context ctxt, Intent intent) {
+			Uri url = downloadmanager.getUriForDownloadedFile(reference);
+			openFile(url.getPath());
+			unregisterReceiver(onComplete);
+		}
+	};
+
+	protected void openFile(String fileName) {
+		Intent target = new Intent(Intent.ACTION_VIEW);
+		target.setDataAndType(Uri.fromFile(new File(fileName)), "application/pdf");
+		startActivity(target);
+
+	}
+
+	private void downloadBook() {
+		// progress = ProgressDialog.show(this, "title", "msg");
+		String servicestring = Context.DOWNLOAD_SERVICE;
+		downloadmanager = (DownloadManager) getSystemService(servicestring);
+		String url = b.getDownloadUrl();
+		Uri uri = Uri.parse(ServiceAddresses.IP + b.getDownloadUrl());
+		DownloadManager.Request request = new Request(uri);
+		String fileName = url.substring(url.lastIndexOf('/') + 1, url.length());
+
+		reference = downloadmanager.enqueue(request.setAllowedNetworkTypes(DownloadManager.Request.NETWORK_WIFI | DownloadManager.Request.NETWORK_MOBILE)
+				.setAllowedOverRoaming(false).setTitle(fileName)
+				.setDescription("File Download")
+				.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, fileName));
+		registerReceiver(onComplete, new IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE));
+
 	}
 
 	private void generateCRCode() {
